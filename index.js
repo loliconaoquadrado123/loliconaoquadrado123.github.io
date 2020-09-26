b34685ef1511b67bc8408fcbcd99286835b5931e
loliconaoquadrado123 <68407584+loliconaoquadrado123@users.noreply.github.com>
2020-09-26 18:44:45 -0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 window.addEventListener('load',e=>{
     document.getElementsByTagName('img')[0].style.height=window.parseInt(innerHeight)+'px';
-    document.getElementById('redirect').addEventListener
+    document.getElementById('redirect').addEventListener('onclick',e=>{
+        window.location.href="https://discord.com/oauth2/authorize?client_id=745726133844312115&scope=bot&permissions=8"
+    });
 })
