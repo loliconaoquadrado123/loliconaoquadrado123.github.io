b66c90b1b2584cf4fc5ec4b4aaf7b1e29c93a239
loliconaoquadrado123 <68407584+loliconaoquadrado123@users.noreply.github.com>
2020-09-26 19:17:09 -0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,9 +1,8 @@
 window.addEventListener('load',e=>{
     document.getElementsByTagName('div')[0].style.height=window.parseInt(innerHeight)+'px';
-    document.getElementById('redirect').addEventListener('onclick',e=>{
-        
-        window.location.href="https://discord.com/oauth2/authorize?client_id=745726133844312115&scope=bot&permissions=8";
-        return ftrue;
+    document.getElementById('redirect').addEventListener('click',e=>{
+        window.location.href="https://discord.com/oauth2/authorize?client_id=745726133844312115&scope=bot&permissions=8",true;
+        console.log('window.location.href')
     });
-    document.getElementById('redirect').style.margin='auto';
+    document.getElementById('redirect').style.marginLeft=window.parseInt(innerHeight)+'px'-document.getElementById('redirect').width;
 })
