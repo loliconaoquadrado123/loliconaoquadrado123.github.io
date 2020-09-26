cc52fa3be348cffd8cd2ba81876fd2ef9a2b9d24
loliconaoquadrado123 <68407584+loliconaoquadrado123@users.noreply.github.com>
2020-09-26 18:40:00 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,4 @@
+window.addEventListener('load',e=>{
+    document.getElementsByTagName('img')[0].style.height=window.parseInt(innerHeight)+'px';
+    document.getElementById('redirect').addEventListener
+})
